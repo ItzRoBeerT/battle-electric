93092f441d8f97cacc8a4b65ac3c0822c1509217
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-07-21 13:16:45 -0400

    Removed gsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: vercelAdapter(),
 });
